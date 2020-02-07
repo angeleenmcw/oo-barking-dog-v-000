be103229ed6bb57f8bc60e8ae5335cdb86480db9
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-07 08:58:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,2 @@
 class Dog 
-  def set_name(new_name) 
-    @name = new_name
-  end
-
-def get_name
-  @name
-end
-
-def name
-  @name
-end
-
-  def bark
-    if new_name = name
-    puts "woof!"
-  end
-end
-
-fido = Dog.new_name
-fido.name = "Fido"
-
-fido.bark
+ 
